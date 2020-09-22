14621db5c762a69b0341d58a0b18671570d0e1e8
orblazer <orblazer.minecraft@gmail.com>
2020-09-22 11:22:55 +0200

    feat(page): implement localized pages

---
+++ src/pages/404.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Index = () => {
+  return (
+    <>
+      <h1>Page not found !</h1>
+      <h2>
+        The page <code>{window.location.pathname}</code> could not be found
+      </h2>
+    </>
+  )
+}
+export default Index
---
+++ src/pages/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Index = () => {
+  return (
+    <>
+      <h1>Hello world !</h1>
+      <h2>
+        You are here : <code>{window.location.pathname}</code>
+      </h2>
+    </>
+  )
+}
+export default Index
